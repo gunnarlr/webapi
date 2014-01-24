1157c79a7cf38dfc152a6924d812bae9f68a3b5e
Gunnar L. Rasmussen <gunnarlr@outlook.com>
2014-01-24 13:47:26 +0100

    initial

---
+++ firstWebApi/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace firstWebApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Trace.WriteLine("Uhey! Here's Gunnar!");
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "This is a test site. Move along :-)";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ firstWebApi/Controllers/ProductsController.cs
@@ -0,0 +1,44 @@
+﻿using firstWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace firstWebApi.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        // Some fake data based of the Product model
+        Product[] products = new Product[] 
+        { 
+            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 }, 
+            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M }, 
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M } 
+        };
+
+        // Be able to get all products in "database"
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        // Get a single document
+        public IHttpActionResult GetProduct(int id)
+        {
+            var cookies = Request.Headers.GetCookies();
+            var numcookies = cookies.Count;
+            Trace.WriteLine("Length " + numcookies + " " + cookies.FirstOrDefault().ToString());
+
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+    }
+}
